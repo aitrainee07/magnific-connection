4bbd79fee830afffafc1cbe1529d3dbde43927cc
aitrainee07 <aitrainee07@gmail.com>
2026-05-16 15:42:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -115,5 +115,10 @@ app.get("/test", async (req, res) => {
     });
 
   }
+ const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on ${PORT}`);
+});
 
 });

2588d33600f10a63ce5c5285d3f24556b5c088c0
aitrainee07 <aitrainee07@gmail.com>
2026-05-16 16:04:22 +0530

    Update automation.js

--- automation.js
+++ automation.js
@@ -1,4 +1,8 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
+
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+
+puppeteer.use(StealthPlugin());
 
 async function runMagnific(prompt) {
 
@@ -17,11 +21,20 @@ async function runMagnific(prompt) {
 
   const page = await browser.newPage();
 
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122 Safari/537.36"
+  );
+
   await page.goto("https://magnific.ai", {
-    waitUntil: "domcontentloaded"
+    waitUntil: "networkidle2",
+    timeout: 60000
   });
 
   const title = await page.title();
+
+  await page.screenshot({
+    path: "magnific.png"
+  });
 
   await browser.close();
 

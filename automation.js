e63a3b19f953b36b59c64056c494c8d85c1504ef
aitrainee07 <aitrainee07@gmail.com>
2026-05-16 14:29:21 +0530

    Update automation.js

--- automation.js
+++ automation.js
@@ -2,10 +2,16 @@ const puppeteer = require("puppeteer");
 
 async function runMagnific(prompt) {
   const browser = await puppeteer.launch({
-    headless: true,
 
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
+  executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+
+  headless: true,
+
+  args: [
+    "--no-sandbox",
+    "--disable-setuid-sandbox"
+  ]
+});
 
   const page = await browser.newPage();
 

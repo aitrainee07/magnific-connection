95dffc3750536a8bb2d9acf2184e9811c8a6659c
aitrainee07 <aitrainee07@gmail.com>
2026-05-16 15:56:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { runMagnific } = require("./automation");
+//const { runMagnific } = require("./automation");
 
 const app = express();
 
